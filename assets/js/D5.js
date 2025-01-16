38c3c567939483a0d13f8d5e9cf172a1ba2a01ad
xAldox995 <valzanialdo@gmail.com>
2025-01-16 18:08:17 +0100

    end the exercice

--- assets/js/D5.js
+++ assets/js/D5.js
@@ -11,19 +11,28 @@ REGOLE
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
+for (let i = 0; i < pets.length; i++) {
+  console.log(pets[i])
+}
+
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
+pets.sort()
+console.log(pets)
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
-
+pets.reverse()
+console.log(pets)
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
 
+pets.push(pets.splice(0, 1)[0])
+console.log(pets)
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
@@ -48,20 +57,53 @@ const cars = [
   },
 ]
 
+for (let i = 0; i < cars.length; i++) {
+  let licensePlate = Math.floor(Math.random() * 100000).toString()
+  cars[i].licensePlate = licensePlate
+}
+console.log(cars)
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+let newCar ={
+  brand: 'Porche',
+  model: 'Cayanne',
+  color: 'black',
+  trims: ['suv', 'style','luxury'],
+  licensePlate: Math.floor(Math.random() * 100000).toString()
+}
 
+cars.push(newCar)
+console.log(cars)
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop()
+}
+console.log(cars)
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = []
 
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0])
+}
+console.log(justTrims)
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].color.startsWith('b')) {
+    console.log('Fizz')
+  } else {
+    console.log('Buzz')
+  }
+}
+
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -69,6 +111,11 @@ const justTrims = []
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
+let i = 0
+while (numericArray[i] !== 32) {
+  console.log(numericArray[i])
+  i++
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
@@ -76,3 +123,73 @@ const numericArray = [
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ['g', 'n', 'u', 'z', 'd']
+const newArray = []
+for (let i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case 'a':
+      newArray.push(1)
+      break
+    case 'b':
+      newArray.push(2)
+      break
+    case 'c':
+      newArray.push(3)
+      break
+    case 'd':
+      newArray.push(4)
+      break
+    case 'e':
+      newArray.push(5)
+      break
+    case 'f':
+      newArray.push(6)
+      break
+    case 'g':
+      newArray.push(7)
+      break
+    case 'h':
+      newArray.push(8)
+      break
+    case 'i':
+      newArray.push(9)
+      break
+    case 'l': 
+      newArray.push(12)
+      break
+    case 'm':
+      newArray.push(13)
+      break
+    case 'n':
+      newArray.push(14)
+      break
+    case 'o':
+      newArray.push(15)
+      break
+    case 'p':
+      newArray.push(16)
+      break
+    case 'q':
+      newArray.push(17)
+      break
+    case 'r':
+      newArray.push(18)
+      break
+    case 's':
+      newArray.push(19)
+      break
+    case 't':
+      newArray.push(20)
+      break
+    case 'u':
+      newArray.push(21)
+      break
+    case 'v': 
+      newArray.push(22)
+      break
+    case 'z':
+      newArray.push(23)
+      break
+  }
+}
+console.log(newArray)
+
